721e8ec9ae2fed2a08e2b1d5ad8b333e0bf18d94
decorelator <fdeveloperf@gmail.com>
2011-05-22 23:28:23 +0300

    DB class
    Signed-off-by: decorelator <fdeveloperf@gmail.com>

--- DB.java
+++ DB.java
@@ -15,8 +15,9 @@ import java.util.HashMap;
 import java.util.Locale;
 
 /**
+ * Класс управления базой данных
  *
- * @author Администратор
+ * @author Плахтий Александр Сергеевич
  */
 public class DB
 {
@@ -26,21 +27,28 @@ public class DB
     private ArrayList<HashMap> resList = new ArrayList();
     private ArrayList<String> columnList = new ArrayList();
     private Connection connection = null;
-    private String connectionConf=null;
-    private String connectionLogin=null;
-    private String connectionPass=null;
+    private String connectionConf = null;
+    private String connectionLogin = null;
+    private String connectionPass = null;
+
+    /**
+     * инициализация соеденения с базой даных, чтение конфигурации
+     * @exception IOException
+     *
+     * Добавил Александр 19.05.2011 10:02
+     */
     protected DB()
     {
         try
         {
-           BufferedReader conf=new BufferedReader(new FileReader(new File("DB.conf")));
-           String host=conf.readLine().split("=")[1];
-           String port=conf.readLine().split("=")[1];
-           String sid=conf.readLine().split("=")[1];
-           connectionLogin=conf.readLine().split("=")[1];
-           connectionPass =conf.readLine().split("=")[1];
-           connectionConf=host+":"+port+":"+sid;
-           conf.close();
+            BufferedReader conf = new BufferedReader(new FileReader(new File("DB.conf")));
+            String host = conf.readLine().split("=")[1];
+            String port = conf.readLine().split("=")[1];
+            String sid = conf.readLine().split("=")[1];
+            connectionLogin = conf.readLine().split("=")[1];
+            connectionPass = conf.readLine().split("=")[1];
+            connectionConf = host + ":" + port + ":" + sid;
+            conf.close();
         } catch (IOException e)
         {
             System.out.println("not found, creating standart conf - 127.0.0.1:1521:XE");
@@ -58,6 +66,12 @@ public class DB
 
     }
 
+    /**
+     * Возвращает  ссылку на класс
+     * @return DB
+     *
+     * Добавил Александр 19.05.2011 10:22
+     */
     public static DB getInstance()
     {
         if (instance == null)
@@ -67,7 +81,15 @@ public class DB
         return instance;
     }
 
-    public boolean openConnection() throws SQLException
+    /**
+     * Открывает соеденеие с базой данных на основе данных из файла конфигурации
+     * @return boolean true если успешно выполнено подключение к БД
+     * @exception ClassNotFoundException на случай ошибки подключения драйвера
+     * @exception SQLException
+     *
+     * Добавил Александр 19.05.2011 10:28
+     */
+    public boolean openConnection()
     {
         try
         {
@@ -77,7 +99,6 @@ public class DB
             Class.forName(driverName);
 
             // Create a connection to the database
-//            String url = "jdbc:oracle:thin:@" + serverName + ":" + portNumber + ":" + sid;
             String url = "jdbc:oracle:thin:@" + connectionConf;
             connection = DriverManager.getConnection(url, connectionLogin, connectionPass);
         } catch (ClassNotFoundException e)
@@ -87,12 +108,19 @@ public class DB
         {
             System.out.println(e.toString());
         }
-        //connection.close();
+
 
         return true;
     }
 
-    public boolean query(String q) throws SQLException
+    /**
+     * Выполняет запрос к базе данных и сохраняет результат в список resList,columnList,rowCount
+     * @return boolean true если если запрос выполнен успешно
+     * @exception SQLException
+     *
+     * Добавил Александр 19.05.2011 13:42
+     */
+    public boolean query(String q)
     {
         try
         {
@@ -122,24 +150,59 @@ public class DB
 
         } catch (SQLException e)
         {
-            System.out.println("DB.Query "+e.toString());
+            System.out.println("DB.Query " + e.toString());
         }
 
         return true;
     }
 
+    /**
+     * возвращает список ассоциативных масивов в которых хранится (key = имя колонки,value = значение колонки)
+     * @return ArrayList<HashMap>
+     *
+     * Добавил Александр 19.05.2011 15:13
+     */
     public ArrayList<HashMap> getResultList()
     {
         return resList;
     }
 
+    /**
+     * возвращает список полученных колонок при выполнении запроса к БД
+     * @return ArrayList
+     *
+     * Добавил Александр 19.05.2011 15:20
+     */
     public ArrayList getColumns()
     {
         return columnList;
     }
 
+    /**
+     * возвращает количество, полученых при выполнении запросса к БД, строк
+     * @return int
+     *
+     * Добавил Александр 19.05.2011 15:22
+     */
     public int getRowcount()
     {
         return rowCount;
+    }
+
+    /**
+     * закрытие соеденения с БД в деструкторе
+     * @return int
+     *
+     * Добавил Александр 22.05.2011 22:11
+     */
+    public void finalize()
+    {
+        try
+        {
+            connection.close();
+        } catch (SQLException ex)
+        {
+            // error manager function here
+        }
     }
 }
