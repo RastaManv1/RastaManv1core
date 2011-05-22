83689202ca4212d2e65351d04eeb8f66b55dedec
decorelator <fdeveloperf@gmail.com>
2011-05-22 23:39:20 +0300

    Buisnes logic class

--- RMCore.java
+++ RMCore.java
@@ -4,61 +4,74 @@
  */
 package logic;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
+ * Класс бизнесс логики
  *
- * @author Администратор
+ * @author Плахтий Александр Сергеевич
  */
 public class RMCore
 {
 
     private static RMCore instance = null;
 
-    private RMCore() //private constructor for singleton pattern
+    /**
+     * Приватный конструктор
+     *
+     * Добавил Александр 18.05.2011 19:22
+     */
+    private RMCore()
     {
     }
-    //call private constructor if instance don't exists and return it
+
+    /**
+     * Статический метод который создаёт объект
+     *
+     * @return RMCore
+     * Добавил Александр 18.05.2011 19:22
+     */
 
     public static RMCore getInstance()
     {
         if (instance == null)
         {
             instance = new RMCore();
-            try
-            {
-                DB.getInstance().openConnection();
-            }
-            catch (SQLException ex)
-            {
-                return null;
-            }
+            DB.getInstance().openConnection();
         }
         return instance;
     }
 
-    //check for existing account with that log and pass
+    /**
+     * Проверка авторизации
+     * @param String login логин пользователя (уникальный)
+     * @param String paswd пароль для логина
+     *
+     * @return String имя пользователля который зарегестрирован под данным логином если операция прошла успешно
+     * и такой пользователь существует, иначе null
+     * Добавил Александр 18.05.2011 19:42
+     */
     public String checkAccount(String login, String paswd)
     {
         String q = null;
         q = "select * from Account where login='" + login + "' and password='" + paswd + "'";
-        try
+        DB.getInstance().query(q);
+        if (DB.getInstance().getRowcount() == 0)
         {
-            DB.getInstance().query(q);
-            if (DB.getInstance().getRowcount() == 0)
-            {
-                return null;//if don't found any then returns null
-            }
-        } catch (SQLException e)
-        {
-            return null;
+            return null;//if don't found any then returns null
         }
         return DB.getInstance().getResultList().get(0).get("NAME").toString(); //if all OK returns name of user
     }
 
-//returns priority of user of last calling checkAccount function
+    /**
+     * Получение приоритета пользователя
+     *
+     * @return int возвращает приоритет пользователя. (адекватно работает после вызова функции авторизаци
+     *      @see checkAccount)
+     *      в случаее ошибки возвращает -1
+     * Добавил Александр 18.05.2011 20:16
+     */
     public int getPriority()
     {
         try
@@ -70,60 +83,51 @@ public class RMCore
         }
     }
 
-//returns priority of user by his login
+    /**
+     * Получение приоритета пользователя
+     *
+     * @return int возвращает приоритет пользователя, который зарегестрирован под указанным логином.
+     *      в случаее ошибки возвращает -1
+     * Добавил Александр 18.05.2011 20:16
+     */
     public int getPriority(String login)
     {
-        try
-        {
-            DB.getInstance().query("select priority from Account where login='" + login + "'");
-            if (DB.getInstance().getRowcount() == 0)
-            {
-                return -1;
-            }
-        } catch (SQLException e)
+        DB.getInstance().query("select priority from Account where login='" + login + "'");
+        if (DB.getInstance().getRowcount() == 0)
         {
             return -1;
         }
         return Integer.valueOf(DB.getInstance().getResultList().get(0).get("PRIORITY").toString());
     }
 
-//returns all resources in the system
+    /**
+     * Возвращает все существующиее ресурсы в системе
+     *
+     * @return ArrayList<HashMap>
+     * Добавил Александр 19.05.2011 17:01
+     */
     public ArrayList<HashMap> getResources()
     {
-        try
-        {
-            if (!DB.getInstance().query("Select title from resources"))
-            {
-                return null;
-            }
-        } catch (SQLException ex)
+        if (!DB.getInstance().query("Select title from resources"))
         {
             return null;
         }
         return DB.getInstance().getResultList();
     }
 
-    //returns all available roles in the system
+    /**
+     * Возвращает все существующиее роли в системе
+     *
+     * @return ArrayList<HashMap>
+     * Добавил Александр 19.05.2011 17:03
+     */
     public ArrayList<HashMap> getRoles()
     {
-        try
-        {
-            if (!DB.getInstance().query("Select title from role"))
-            {
-                return null;
-            }
-        } catch (SQLException ex)
+        if (!DB.getInstance().query("Select title from role"))
         {
             return null;
         }
         return DB.getInstance().getResultList();
     }
 
-    public void reReserve(String accID, String start, String end)
-    {
-//        SimpleDateFormat sd=new SimpleDateFormat("MM-dd:HH:mm");
-//        System.out.println(sd.format(d));
-        // DB.query("select login, start,end from Account a,Journal j where a.acc_id=j.acc_id and start<=todate("+start+") "+
-        //         "or start<todate("+end+")");
-    }
 }
